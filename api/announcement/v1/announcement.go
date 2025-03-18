6b532792f96b832d39a0e2b8723552d7505c7425
BitBolt19 <2084779014@qq.com>
2025-03-18 17:09:45 +0800

    公告  api  修改2

--- api/announcement/v1/announcement.go
+++ api/announcement/v1/announcement.go
@@ -7,9 +7,11 @@ import (
 
 type GetNewsReq struct {
 	g.Meta `path:"/announcement" tags:"公告" method:"get" summary:"公告列表"`
-	Type   uint `json:"type" dc:"公告类型（1.滚动图片；2.首页公告；3.新闻资讯；4.脚部公告；5.系统公告；6.全屏公告）"`
-	Page   int  `json:"page" in:"query" d:"1"  v:"min:1#分页号码错误"     dc:"分页号码，默认1"`
-	Size   int  `json:"size" in:"query" d:"20" v:"max:20#分页数量最大20条" dc:"分页数量，最大20"`
+	Type   int `json:"type" dc:"公告类型: 1:公告 2:帮助中心 3.首页弹出 4.binner" `
+	//Type     uint   `json:"type" dc:"公告类型（1.滚动图片；2.首页公告；3.新闻资讯；4.脚部公告；5.系统公告；6.全屏公告）"`
+	Language string `json:"language" dc:"语言"`
+	Page     int    `json:"page" in:"query" d:"1"  v:"min:1#分页号码错误"     dc:"分页号码，默认1"`
+	Size     int    `json:"size" in:"query" d:"20" v:"max:20#分页数量最大20条" dc:"分页数量，最大20"`
 }
 
 type GetNewsRes struct {
